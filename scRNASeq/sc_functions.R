9a71a6bed3ecd6b0a2cdc6c5a94b7c93c58a6839
Sameet Mehta <sameet.mehta@yale.edu>
2024-09-23 10:04:02 -0400

    ADding more useful function

--- scRNASeq/sc_functions.R
+++ scRNASeq/sc_functions.R
@@ -8,28 +8,17 @@ suppressPackageStartupMessages(library(patchwork))
 suppressPackageStartupMessages(library(pheatmap))
 suppressPackageStartupMessages(library(glmGamPoi))
 
-make_opdir <- function(project_name) {
-  # Create output directory to hold all the graphs generated for each sample
-  of_dir <- paste(project_name, "_analysis", sep = "")
-  command <- paste("mkdir ", of_dir, sep = "")
-  system(command)
-  return(of_dir)
-}
-
-process_sample <- function(dir_name, project_name = "scRNA") {
+process_h5 <- function(fn, p_name) {
   
+  of_dir <- make_opdir(p_name)
+  ofn <- file.path(of_dir, paste(paste(p_name, 
+                                       Sys.Date(), 
+                                       sep = ""), 
+                                 ".RDS", sep = ""))
   
-  of_dir <- make_opdir(dir_name)
+  sce <- CreateSeuratObject(Read10X_h5(fn),
+                            project = p_name)
   
-  
-  # create an output path for the sample
-  ofn <- paste(paste(project_name, Sys.Date(), sep = "-"), ".RDS", sep = "")
-  ofn <- file.path(of_dir, ofn)
-  print(ofn)
-  
-  # Read in the count matrix from cellranger output
-  exp_mat <- Read10X(dir_name)
-  sce <- CreateSeuratObject(exp_mat, project = project_name)
   sce[["percent.mito"]] <- PercentageFeatureSet(sce, pattern = "^MT-")
   
   # plot QC
@@ -43,21 +32,89 @@ process_sample <- function(dir_name, project_name = "scRNA") {
   
   sce <- SCTransform(sce, method = "glmGamPoi", vars.to.regress = "percent.mito", verbose = F)
   sce <- FindVariableFeatures(sce, selection.method = "vst", nfeatures = 2000)
-  sce <- RunPCA(sce) |>
-    FindNeighbors(reduction = "pca", dims = 1:30) %>%
-    RunUMAP(dims = 1:30) %>%
+  sce <- RunPCA(sce, verbose = F) |>
+    FindNeighbors(reduction = "pca", dims = 1:30, verbose = F) %>%
+    RunUMAP(dims = 1:30, verbose = F) %>%
     FindClusters(verbose = FALSE)
   saveRDS(sce, ofn)
   return(sce)
 }
 
-integrate_sce <- function(sce_list) {
+make_opdir <- function(project_name) {
+  # Create output directory to hold all the graphs generated for each sample
+  of_dir <- paste(project_name, "_analysis", sep = "")
+  message(paste("Saving to ", of_dir))
+  command <- paste("mkdir ", of_dir, sep = "")
+  system(command)
+  return(of_dir)
+}
+
+process_sample <- function(dir_name, project_name = "scRNA") {
+
+
+  of_dir <- make_opdir(project_name)
+
+
+  # create an output path for the sample
+  ofn <- file.path(of_dir, paste(paste(project_name, Sys.Date(), sep = ""), ".RDS", sep = ""))
+  # ofn <- paste(paste(project_name, Sys.Date(), sep = "-"), ".RDS", sep = "")
+  # ofn <- file.path(of_dir, ofn)
+  print(ofn)
+
+  # Read in the count matrix from cellranger output
+  exp_mat <- Read10X(dir_name)
+  sce <- CreateSeuratObject(exp_mat, project = project_name)
+  sce[["percent.mito"]] <- PercentageFeatureSet(sce, pattern = "^MT-")
+
+  # plot QC
+  p <- VlnPlot(sce, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
+  ggsave(file.path(of_dir, "voilin_plot_QC.pdf"), plot = p, width = 14, height = 7, units = "in")
+
+  p2 <- FeatureScatter(sce, feature1 = "nCount_RNA", feature2 = "percent.mito")
+  p3 <- FeatureScatter(sce, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+  p4 <- p2 + p3 + plot_layout(guides = "collect")
+  ggsave(file.path(of_dir, "qc-plot-2.pdf"), plot = p4, width = 14, height = 7, units = "in")
+
+  sce <- SCTransform(sce, method = "glmGamPoi", vars.to.regress = "percent.mito", verbose = F)
+  sce <- FindVariableFeatures(sce, selection.method = "vst", nfeatures = 2000)
+  sce <- RunPCA(sce, verbose = F) |>
+    FindNeighbors(reduction = "pca", dims = 1:30, verbose = F) %>%
+    RunUMAP(dims = 1:30, verbose = F) %>%
+    FindClusters(verbose = FALSE)
+  saveRDS(sce, ofn)
+  return(sce)
+}
+
+integrate_sce <- function(sce_list, ...) {
   use_features <- SelectIntegrationFeatures(object.list = sce_list)
   use_sce_list <- lapply(sce_list, function(x) {
     x <- ScaleData(x, features = use_features, verbose = FALSE)
     x <- RunPCA(x, features = use_features, verbose = FALSE)
   })
   use_anchors <- FindIntegrationAnchors(object.list = use_sce_list, anchor.features = use_features)
+  combined <- IntegrateData(anchorset = use_anchors, ...)
+
+  DefaultAssay(combined) <- "integrated"
+
+  combined <- ScaleData(combined, verbose = FALSE)
+  combined <- RunPCA(combined, npcs = 30, verbose = FALSE)
+  combined <- RunUMAP(combined, reduction = "pca", dims = 1:30)
+  combined <- FindNeighbors(combined, reduction = "pca", dims = 1:30)
+  combined <- FindClusters(combined, resolution = 0.5)
+  return(combined)
+}
+
+integrate_sce_2 <- function(x, y) {
+  use_sce_l <- list(x, y)
+  use_features <- SelectIntegrationFeatures(object.list = use_sce_l, 
+                                            verbose = F)
+  use_sce_list <- lapply(use_sce_l, function(sc) {
+    sc <- ScaleData(sc, features = use_features, verbose = FALSE)
+    sc <- RunPCA(sc, features = use_features, verbose = FALSE)
+  })
+  use_anchors <- FindIntegrationAnchors(object.list = use_sce_list,
+                                        anchor.features = use_features,
+                                        reduction = "rpca")
   combined <- IntegrateData(anchorset = use_anchors)
   
   DefaultAssay(combined) <- "integrated"
@@ -69,3 +126,5 @@ integrate_sce <- function(sce_list) {
   combined <- FindClusters(combined, resolution = 0.5)
   return(combined)
 }
+
+# make_phate <-
